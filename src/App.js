2891ac40da1c5cc95eba11737a409577ceffd738
evaferreira <evaferreira92@hotmail.com>
2022-12-12 03:44:46 -0300

    ejemplo eventos y hooks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import { useState } from 'react';
 import CardWrapper from './components/CardWrapper';
 
 function App() {
+  const [cantidad, setCantidad] = useState(0);
+  const [mostrar, setMostrar] = useState(false);
+
+  function handleMostrar () {
+    setMostrar(!mostrar)
+  }
+
+  // function aaah () {
+  //   alert('te saludan');
+  // }
+
   return (
     <div className="App">
+      <button onClick={() => setCantidad(cantidad - 1)}>
+        -
+      </button>
+      {cantidad}
+      <button onClick={() => setCantidad(cantidad + 1)}>
+        +
+      </button>
+
+      {mostrar &&
+        <h2>holis</h2>
+      }
+      <button onClick={handleMostrar}>
+        Mostrar
+      </button>
       <CardWrapper
         titulo={'Ofertas'}
       />
