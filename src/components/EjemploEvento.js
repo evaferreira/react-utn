3d4ca99e1c59dcbe82a7c04aee9416637281e561
evaferreira <evaferreira92@hotmail.com>
2022-12-13 21:25:09 -0300

    Eventos / onClick / funciones

---
+++ src/components/EjemploEvento.js
@@ -0,0 +1,13 @@
+
+export default function EjemploEvento () {
+    // Declaro la función dentro del componente
+    // function saludar () {
+    //     alert('Hola!');
+    // }
+
+    return (
+        <button onClick={() => alert('holiiiis')}>
+            Cliqueame
+        </button>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import CardWrapper from "../components/CardWrapper"
+import EjemploEvento from "../components/EjemploEvento"
 
 export default function Home () {
     return (
         <>
             <h1>Estás en la home</h1>
+            <EjemploEvento />
             <CardWrapper
                 titulo={'Ofertas'}
             />
